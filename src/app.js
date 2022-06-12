2651932e4cd0cac0493cfe8c67e90b7d42c7026d
BPasse <104333390+BerryPasse@users.noreply.github.com>
2022-06-11 20:49:20 -0500

    Eliminated celsius conversion
    Focus on Farenheit temp only due to geographical location.

--- src/app.js
+++ src/app.js
@@ -63,7 +63,7 @@ function displayForecast(response) {
 }
 function getForecast(coordinates) {
   let apiKey = "5d7ac2056c0edbd0916ff4c0375b0145";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -95,7 +95,7 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "5d7ac2056c0edbd0916ff4c0375b0145";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
@@ -113,22 +113,11 @@ function displayFahrenheitTemperature(event) {
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Miami");
 
